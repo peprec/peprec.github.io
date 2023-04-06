27c0f4ae3f023588e70e9856e9c0265e8f10ddc0
Pamir Erdeniz <seda@frizbit.com>
2023-04-06 20:55:48 +0200

    edited  dashboard

--- assets/demo/chart-bar-demo.js
+++ assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Matrix Factorization", "CoClustering","KNN-UserBased", "DeepLearnig"],
     datasets: [{
-      label: "Revenue",
+      label: "Precision@k",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [0.85, 0.78, 0.82, 0.80],
     }],
   },
   options: {
@@ -30,8 +30,52 @@ var myLineChart = new Chart(ctx, {
       }],
       yAxes: [{
         ticks: {
-          min: 0,
-          max: 15000,
+          min: 0.7,
+          max: 1,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart2");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["KNN-ItemBased", "TopN", "TopN-InCategory"],
+    datasets: [{
+      label: "Precision@k",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [0.85, 0.82, 0.80],
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0.7,
+          max: 1,
           maxTicksLimit: 5
         },
         gridLines: {
